b47ac0613354cb05c10cbae7894c95c82a1f7566
R-vanLangeveld <ryndee25@gmail.com>
2024-05-21 13:06:40 -0600

    Completed Learning05

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square square = new Square("red", 4);
+        Rectangle rectangle =  new Rectangle("blue", 5, 9);
+        Circle circle = new Circle("yellow", 5);
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine(shape.GetArea());
+        }
     }
 }
